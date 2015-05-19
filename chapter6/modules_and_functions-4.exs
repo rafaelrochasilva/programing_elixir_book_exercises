854211f7e878669d9980122d74ec61ce427dced9
Rafael Rocha <rafaelrochasilva@gmail.com>
2015-05-19 08:34:51 -0300

    Add a fill more exercises

--- chapter6/modules_and_functions-4.exs
+++ chapter6/modules_and_functions-4.exs
@@ -2,7 +2,16 @@ defmodule Calculator do
   def sum(0) do
     0
   end
+
   def sum(n) do
-    n + sum(n-1)
+    IO.inspect n + sum(n-1)
+  end
+
+  def sum_list([]) do
+    0
+  end
+
+  def sum_list([_head|tail]) do
+    _head + sum_list(tail)
   end
 end
