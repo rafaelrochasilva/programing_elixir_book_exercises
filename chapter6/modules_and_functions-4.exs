5e5dc7830a395a6528a0c312ef8f004627d8192a
Rafael Rocha <rafaelrochasilva@gmail.com>
2015-05-20 07:38:31 -0300

    Sum list with acc

--- chapter6/modules_and_functions-4.exs
+++ chapter6/modules_and_functions-4.exs
@@ -14,4 +14,12 @@ defmodule Calculator do
   def sum_list([_head|tail]) do
     _head + sum_list(tail)
   end
+
+  def sum_with_state([], total) do
+    total
+  end
+
+  def sum_with_state([_head|tail], total) do
+    sum(tail, head+total)
+  end
 end
