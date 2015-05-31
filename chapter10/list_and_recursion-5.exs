4424f4b7da87f221a79283c45a0ce7a5d64889d1
Rafael Rocha <rafaelrochasilva@gmail.com>
2015-05-31 10:00:54 -0300

    Add  split exercise

--- chapter10/list_and_recursion-5.exs
+++ chapter10/list_and_recursion-5.exs
@@ -10,4 +10,21 @@ defmodule MyList do
       false
     end
   end
+
+  def each([], _func) do
+    []
+  end
+
+  def each([head|tail], func) do
+    [ func.(head) | each(tail, func) ]
+  end
+
+  def split(list, count),      do: _split(list, [], count)
+  defp _split([], front, _),   do: [ Enum.reverse(front), [] ]
+  defp _split(tail, front, 0), do: [ Enum.reverse(front), tail ]
+  defp _split([ head | tail ], front, count)  do
+    _split(tail, [head|front], count-1)
+  end
+
+  def take(list, n), do: hd(split(list, n))
 end
